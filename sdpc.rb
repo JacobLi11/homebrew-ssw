a73c395f06b01d8a76c5872c6e2f5be7648a8c4f
JacobLi11 <JacobLi11@users.noreply.github.com>
2023-05-10 08:27:56 +0000

    add sdpc version

--- sdpc.rb
+++ sdpc.rb
@@ -14,7 +14,7 @@ class Sdpc < Formula
     if Hardware::CPU.arm?
       url https://s3.us-west-2.amazonaws.com/splashlock-assets.splashshield.ai/sdpc/stage/linux/amd64/sdpc-linux-arm64-0.1.39-stage-0.0.3.34.zip
       sha256 f8cfd366300fecac9c668d024422f795fe7c54216443a83aaf9e30f7125130c9
-  
+
       def install
         bin.install sdpc-darwin-arm64
         # Install bash completion
@@ -28,7 +28,7 @@ class Sdpc < Formula
     if Hardware::CPU.intel?
       url https://s3.us-west-2.amazonaws.com/splashlock-assets.splashshield.ai/sdpc/stage/linux/amd64/sdpc-linux-amd64-0.1.39-stage-0.0.3.34.zip
       sha256 560225d17e8c560accba4cf92cdee0628906e9eabca6b938414a5df0abc6f6f8
-  
+
       def install
         bin.install sdpc-darwin-arm64
         # Install bash completion
